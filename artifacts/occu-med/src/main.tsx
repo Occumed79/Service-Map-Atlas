0ae35b1e3e5dc5848dd81ddbca0c31ddc0e0d7eb
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-15 02:54:42 -0700

    Load client map fixes

--- artifacts/occu-med/src/main.tsx
+++ artifacts/occu-med/src/main.tsx
@@ -4,5 +4,6 @@ import "./index.css";
 import "./client-polish.css";
 import "./admin-theme.css";
 import "./brand-overrides.css";
+import "./client-map-fixes.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
