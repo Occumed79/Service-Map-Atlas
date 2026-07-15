0b790f1b4a176217133e26ff98a72adf424a7bb4
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-15 01:46:27 -0700

    Add separate client user management page

---
+++ artifacts/occu-med/src/pages/admin/client-users.tsx
@@ -0,0 +1,5 @@
+import { CredentialUserManager } from "@/components/credential-user-manager";
+
+export default function AdminClientUsers() {
+  return <CredentialUserManager audience="client" />;
+}
