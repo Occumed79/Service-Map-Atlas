73db3148c1a97370f555af0f98e084cec81e8ab2
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-15 01:19:52 -0700

    Load client Atlas polish styles

--- artifacts/occu-med/src/main.tsx
+++ artifacts/occu-med/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./client-polish.css";
 import "./admin-theme.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
