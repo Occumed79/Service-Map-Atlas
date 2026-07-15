780d1dc1ac55c39ef7d064e30d471a3383ffe79a
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-15 01:50:42 -0700

    Load supplied logo styling

--- artifacts/occu-med/src/main.tsx
+++ artifacts/occu-med/src/main.tsx
@@ -3,5 +3,6 @@ import App from "./App";
 import "./index.css";
 import "./client-polish.css";
 import "./admin-theme.css";
+import "./brand-overrides.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
