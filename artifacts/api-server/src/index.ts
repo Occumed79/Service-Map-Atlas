41e3d12bc00e4f5c7dc3a7d28c60d83d5aecad25
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-08 00:13:10 -0700

    Run corrected Atlas source bootstrap on admin startup

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import { logger } from "./lib/logger";
 import { runCorrectedAtlasDatasetBootstrap } from "./services/atlasCorrectedDatasetBootstrap";
+import { runAtlasSourceDatasetBootstrap } from "./services/atlasSourceDatasetBootstrap";
 
 const rawPort = process.env["PORT"];
 
@@ -18,6 +19,7 @@ if (Number.isNaN(port) || port <= 0) {
 
 async function start() {
   await runCorrectedAtlasDatasetBootstrap();
+  await runAtlasSourceDatasetBootstrap();
 
   app.listen(port, (err) => {
     if (err) {
